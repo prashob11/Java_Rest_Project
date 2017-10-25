0260d16cec432cab0751b5aae77a35c0f396b5a6
Prashob Nair <Prashob.Nair@smu.ca>
2017-10-25 11:17:08 -0300

    created tables for login and connected to db

--- Hotel_Reservations/Models/IdentityModels.cs
+++ Hotel_Reservations/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Hotel_Reservations.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ModelReservations", throwIfV1Schema: false)
         {
         }
 
