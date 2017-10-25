58537a9b019d69d5e12f0903a394ff51d8e07be4
Nikita <nikita.neveditsin@smu.ca>
2017-10-25 11:32:38 -0300

    credit card exp date validation and formatting added

---
+++ Hotel_Reservations/Validators/CreditCardExpDateValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Reservations.Validators
+{
+    public class CreditCardExpDateValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime expdate = (DateTime)value;
+
+            if (expdate < DateTime.Today)
+            {
+                return new ValidationResult("The card is expired");
+            }
+
+            if (expdate.Year > 2031 || expdate.Year < 2017)
+            {
+                return new ValidationResult("Year should be between 2017 and 2031");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
