fff4945d14fa1c2463cd8ccfaea852a8ed1350e8
Prashob Nair <Prashob.Nair@smu.ca>
2017-10-30 00:16:02 -0300

    Page access restriction configured

--- Hotel_Reservations/Controllers/HomeController.cs
+++ Hotel_Reservations/Controllers/HomeController.cs
@@ -8,11 +8,12 @@ namespace Hotel_Reservations.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
