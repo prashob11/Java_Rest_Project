cca7a127432e0040f3e5fa555bf7748c5f730687
Nikita <nikita.neveditsin@smu.ca>
2017-11-21 11:54:06 -0400

    the project connected to DB, get all countries implemented. Project structure changed

---
+++ Java_REST/javarest/src/main/java/entities/Country.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Countries")
+public class Country implements Serializable {
+	private static final long serialVersionUID = 1767113837094634625L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "countryId")
+	private int countryId;
+
+	@Column(name = "country")
+	private String country;
+
+	@Column(name = "postalPattern")
+	private String postalPattern;
+
+	public int getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostalPattern() {
+		return postalPattern;
+	}
+
+	public void setPostalPattern(String postalPattern) {
+		this.postalPattern = postalPattern;
+	}
+
+}
