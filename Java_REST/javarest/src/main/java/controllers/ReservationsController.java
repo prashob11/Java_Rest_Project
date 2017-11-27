8e8d4b75b49e7662a1afa56f5eece53c627e7d79
Nikita <nikita.neveditsin@smu.ca>
2017-11-27 10:23:50 -0400

    reservations: PUT and DELETE requests added

--- Java_REST/javarest/src/main/java/controllers/ReservationsController.java
+++ Java_REST/javarest/src/main/java/controllers/ReservationsController.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import dao.ReservationDAO;
 import entities.Reservation;
@@ -22,5 +28,18 @@ public class ReservationsController {
         return dao.getReservations();
     }
 	
-
+	@DELETE
+	@Path("/{id}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response deleteReservation(@PathParam("id") int id) {
+		boolean deleted = dao.deleteReservation(id);
+		return Response.status(deleted ? Response.Status.OK : Response.Status.NO_CONTENT).build();
+	}
+	
+	@PUT
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public Serializable createReservation(Reservation r) {		
+		return dao.createReservation(r);
+	}
 }
