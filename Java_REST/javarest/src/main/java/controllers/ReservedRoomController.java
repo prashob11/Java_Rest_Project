9a56a1c245c44a5fe60f28d424ef84d8aea5e73b
Nikita <nikita.neveditsin@smu.ca>
2017-11-30 13:55:57 -0400

    reservedRooms: additional API

--- Java_REST/javarest/src/main/java/controllers/ReservedRoomController.java
+++ Java_REST/javarest/src/main/java/controllers/ReservedRoomController.java
@@ -1,12 +1,17 @@
 package controllers;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import dao.ReservedRoomDAO;
 import entities.ReservedRoom;
@@ -18,14 +23,29 @@ public class ReservedRoomController {
 	
 	@GET
     @Produces({ MediaType.APPLICATION_JSON })
-    public List<ReservedRoom> getReservations() {
+    public List<ReservedRoom> getReservedRooms() {
         return dao.getReservedRooms();
     }
 	
 	@GET
     @Path("/{reservationId}")
     @Produces({ MediaType.APPLICATION_JSON })
-    public List<ReservedRoom> getRegionsByCountryId(@PathParam("reservationId") int reservationId) {
+    public List<ReservedRoom> getReservedRoomsByReservationId(@PathParam("reservationId") int reservationId) {
 		return dao.getReservedRoomsByReservationId(reservationId);	
     }
+	
+	@DELETE
+	@Path("/{id}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response deleteReservedRoom(@PathParam("id") int id) {
+		dao.deleteReservedRoom(id);
+		return Response.status(Response.Status.OK).build();
+	}
+	
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public Serializable createReservedRoom(ReservedRoom rr) {		
+		return dao.createReservedRoom(rr);
+	}
 }
